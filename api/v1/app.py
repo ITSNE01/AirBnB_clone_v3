f0e0b07063b9659a50e39c5a0a49d186f11edc79
mery16em <ml.meryem.16@gmail.com>
2025-02-16 12:36:53 -0800

    merge master

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,36 @@
-# api/v1/app.py
+#!/usr/bin/python3
+"""Flask server (variable app)"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Register the Blueprint
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
-# Define teardown function to close storage session
+
 @app.teardown_appcontext
-def teardown(exc):
+def downtear(self):
+    """A method that Closes the storage"""
     storage.close()
 
-if __name__ == "__main__":
-    # Set default values for host and port if not set in environment variables
-    app.run(
-        host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-        port=int(os.getenv('HBNB_API_PORT', 5000)),
-        threaded=True
-    )
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """This method is called if page not found"""
+    return jsonify({'error': 'Not found'}), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,24 @@
-# api/v1/views/index.py
-
-from api.v1.views import app_views
+#!/usr/bin/python3xx
+'''api status'''
+import models
+from models import storage
+from models.base_model import BaseModel
 from flask import jsonify
+from api.v1.views import app_views
 
-# Define route for /status
-@app_views.route('/status', methods=['GET'])
-def status():
-    """Return the status of the API"""
-    return jsonify({"status": "OK"})
 
+@app_views.route('/status', strict_slashes=False)
+def returnstuff():
+    '''return stuff'''
+    return jsonify(status='OK')
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stuff():
+    '''JSON Responses'''
+    todos = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todos:
+        todos[key] = storage.count(todos[key])
+    return jsonify(todos)
